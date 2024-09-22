17a7538c98d3f68f72ae9dd442b7b1a2b92b01d6
vunt <vunguyentien10@gmail.com>
2024-09-22 23:09:57 +0700

    [Update] connected db

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // index.js
 import app from './src/app.js';
+import {connect} from './src/database/connect.js';
 
 const PORT = process.env.PORT || 3000;
+
+connect();
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
