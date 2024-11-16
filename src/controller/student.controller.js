cd3dd0e8144a2eda6d31bd8d3dded5baa8fe5d56
vunt <vunguyentien10@gmail.com>
2024-11-16 18:10:14 +0700

    [Update] more validate when create or update student

--- src/controller/student.controller.js
+++ src/controller/student.controller.js
@@ -1,5 +1,6 @@
 import {createOrUpdate} from '../helpers/createOrUpdate.js';
 import StudentModel from '../model/student.model.js';
+import {ValidationError} from 'sequelize';
 
 export const createOrUpdateStudent = async (req, res) => {
   try {
@@ -20,6 +21,16 @@ export const createOrUpdateStudent = async (req, res) => {
     });
   } catch (err) {
     console.error(`Error during create or update student`, err);
+
+    if (err instanceof ValidationError) {
+      const errorMessages = err.errors.map((error) => error.message);
+      return res.status(400).json({
+        success: false,
+        message: 'Validation error',
+        errors: errorMessages,
+      });
+    }
+
     return res.status(500).json({
       success: false,
       message: 'Internal server error',
