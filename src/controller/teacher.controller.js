cbe2dd95ed357edb9c48fce0afe177fff86a6c93
vunt <vunguyentien10@gmail.com>
2024-11-16 17:57:47 +0700

    [Update] creat teacher

--- src/controller/teacher.controller.js
+++ src/controller/teacher.controller.js
@@ -1,5 +1,6 @@
 import {createOrUpdate} from '../helpers/createOrUpdate.js';
 import TeacherModel from '../model/teacher.model.js';
+import {ValidationError} from 'sequelize';
 
 export const createOrUpdateTeacher = async (req, res) => {
   try {
@@ -20,6 +21,16 @@ export const createOrUpdateTeacher = async (req, res) => {
     });
   } catch (err) {
     console.error(`Error during create or update teacher`, err);
+
+    if (err instanceof ValidationError) {
+      const errorMessages = err.errors.map((error) => error.message);
+      return res.status(400).json({
+        success: false,
+        message: 'Validation error',
+        errors: errorMessages,
+      });
+    }
+
     return res.status(500).json({
       success: false,
       message: 'Internal server error',
--- src/route/teacher.route.js
+++ src/route/teacher.route.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import {createOrUpdateTeacher} from '../controller/teacher.controller.js';
-import {requiredField} from '../middleware/requiredField.js';
-import TeacherModel from '../model/teacher.model.js';
 
 const router = express.Router();
 
-router.post('/', requiredField(TeacherModel), createOrUpdateTeacher);
+router.post('/', createOrUpdateTeacher);
 
 export default router;
